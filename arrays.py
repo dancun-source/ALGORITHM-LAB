b5a013f69fde61334dcd9ff4420e14c2b9099c92
Dancun <dancunyego975@gmail.com>
2026-02-09 21:14:41 +0300

    Create arrays.py

---
+++ arrays.py
@@ -0,0 +1,12 @@
+# Arrays.py
+# Static Linear Data Structure: Array
+
+# Example: storing student marks
+marks = [70, 85, 90, 60]
+
+print("Array Elements:")
+for mark in marks:
+    print(mark)
+
+# Access element by index
+print("First element:", marks[0])
